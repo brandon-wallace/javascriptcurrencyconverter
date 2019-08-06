afb2607770c53bb420eb2bb3ea6e47f86aa88f2f
bwallace <17948072+brandon-wallace@users.noreply.github.com>
2019-08-06 13:24:08 -0400

    Add routes

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ app.set('views', viewPath);
 app.use(express.static(publicPath));
 
 
+// Routes
 app.get('/', (req, res) => {
     res.render('index', {title: 'currency converter'});
 });
@@ -39,11 +40,26 @@ app.get('/tools', (req, res) => {
 });
 
 
+app.get('/trading', (req, res) => {
+    res.render('trading', {title: 'trading'});
+});
+
+
+app.get('/resources', (req, res) => {
+    res.render('resources', {title: 'resources'});
+});
+
+
+app.get('/transfer', (req, res) => {
+    res.render('transfer', {title: 'transfer'});
+});
+
+
 app.get('*', (req, res) => {
     res.render('404', {title: 'page not found'});
 });
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server listening on ${port}.`);
 });
